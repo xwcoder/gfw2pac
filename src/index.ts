16ef70d5a1c9d53b195dd7710a418c0c80201026
creep <creep.wei@gmail.com>
2019-01-06 20:36:53 +0800

    add white list

--- src/index.ts
+++ src/index.ts
@@ -25,59 +25,97 @@ function renderFile (filepath, options): string {
 }
 
 // tslint:disable:cognitive-complexity
+function parseRule (rule) {
+
+  const hostR = []
+  const urlR = []
+  const regR = []
+
+  if (rule[0] === '|' && rule[1] === '|') {
+
+    rule = rule.slice(2)
+
+    if (rule[0] === '.') {
+      hostR.push(`*${rule}`)
+    } else {
+      hostR.push(rule)
+      hostR.push(`*.${rule}`)
+    }
+
+  } else if (rule[0] === '|') {
+
+    urlR.push(rule.slice(1))
+
+  } else if (rule[0] === '/') {
+
+    regR.push(rule.slice(1).slice(0, -1))
+
+  } else {
+
+    if (rule[0] === '.') {
+      rule = `*${rule}`
+    }
+
+    if (rule.includes('/')) {
+      urlR.push(rule)
+    } else if (rule.includes('.')) {
+
+      hostR.push(rule)
+
+      if (rule[0] !== '*') {
+        hostR.push(`*.${rule}`)
+      }
+    } else {
+      urlR.push(rule)
+    }
+  }
+
+  return {
+    hostR,
+    urlR,
+    regR
+  }
+}
+
 export async function gfw2pac (): Promise<void> {
 
-  const hostRules: string[] = []
-  const urlRules: string[] = []
-  const regRules: string[] = []
+  let hostRules: string[] = []
+  let urlRules: string[] = []
+  let regRules: string[] = []
+  let whiteHostRules: string[] = []
+  let whiteUrlRules: string[] = []
+  let whiteRegRules: string[] = []
 
   const { data } = await axios.get(url)
   const lines: string[] = Buffer.from(data, 'base64').toString('utf8').split('\n')
 
-  for (let rule of lines) {
+  for (const rule of lines) {
+    let hostR
+    let urlR
+    let regR
 
-    if (rule[0] === '|' && rule[1] === '|') {
+    if (rule[0] === '@' && rule[1] === '@') {
+      ({ hostR, urlR, regR } = parseRule(rule.slice(2)))
+      whiteHostRules = [...whiteHostRules, ...hostR]
+      whiteUrlRules = [...whiteUrlRules, ...urlR]
+      whiteRegRules = [...whiteRegRules, ...regR]
+    } else if (rule && !['[', '!'].includes(rule[0])) {
+      ({hostR, urlR, regR} = parseRule(rule))
 
-      rule = rule.slice(2)
-
-      if (rule[0] === '.') {
-        hostRules.push(`*${rule}`)
-      } else {
-        hostRules.push(rule)
-        hostRules.push(`*.${rule}`)
-      }
-
-    } else if (rule[0] === '|') {
-
-      rule = rule.slice(1)
-      urlRules.push(rule)
-
-    } else if (rule[0] === '/') {
-
-      regRules.push(rule.slice(1).slice(0, -1))
-
-    } else if (rule && !['[', '!', '@'].includes(rule[0])) {
-
-      if (rule[0] === '.') {
-        rule = `*${rule}`
-      }
-
-      if (rule.includes('/')) {
-        urlRules.push(rule)
-      } else if (rule.includes('.')) {
-
-        hostRules.push(rule)
-        if (rule[0] !== '*') {
-          hostRules.push(`*.${rule}`)
-        }
-
-      } else {
-        urlRules.push(rule)
-      }
+      hostRules = [...hostRules, ...hostR]
+      urlRules = [...urlRules, ...urlR]
+      regRules = [...regRules, ...regR]
     }
   }
 
-  const pac = renderFile(path.resolve(__dirname, '../proxy.pac.ejs'), {hostRules, urlRules, regRules})
+  const pac = renderFile(path.resolve(__dirname, '../proxy.pac.ejs'), {
+    hostRules,
+    urlRules,
+    regRules,
+    whiteHostRules,
+    whiteUrlRules,
+    whiteRegRules
+  })
 
   fs.writeFileSync(path.resolve(__dirname, '../proxy.pac'), pac)
 
